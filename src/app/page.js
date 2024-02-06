3c57e2b09483153c55c74742ac08e79c6a20d556
SaifullaJubair <saifullajubair6@gmail.com>
2024-02-07 00:55:27 +0600

    material ui install

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
+import { Button } from "@mui/material";
+
 const HomePage = () => {
-  return <div>Welcome to Our Charity</div>;
+  return (
+    <div>
+      Welcome to Our Charity
+      <Button variant="contained" color="secondary" className="bg-red-500 m-16">
+        This is button
+      </Button>
+    </div>
+  );
 };
 
 export default HomePage;
