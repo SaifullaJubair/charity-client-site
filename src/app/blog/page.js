425a914974fb24a20dbe99fb3ce46fa311c494a5
SaifullaJubair <saifullajubair6@gmail.com>
2024-02-09 21:23:50 +0600

    event and blog route

---
+++ src/app/blog/page.js
@@ -0,0 +1,12 @@
+import Blog from "@/components/ui/Blog/Blog";
+import React from "react";
+
+const BlogPage = () => {
+  return (
+    <div>
+      <Blog />
+    </div>
+  );
+};
+
+export default BlogPage;
---
+++ src/app/event/page.js
@@ -0,0 +1,11 @@
+import OurEvents from "@/components/ui/OurEvents/OurEvents";
+
+const Event = () => {
+  return (
+    <div>
+      <OurEvents />
+    </div>
+  );
+};
+
+export default Event;
