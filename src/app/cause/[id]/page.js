c11e05c4060588bc86cad42c3b7fb1798d19bcb9
SaifullaJubair <saifullajubair6@gmail.com>
2024-02-11 06:30:40 +0600

    donate validate

--- src/app/cause/[id]/page.js
+++ src/app/cause/[id]/page.js
@@ -6,6 +6,7 @@ import {
 } from "@/redux/api/apiSlice";
 import Loader from "@/utils/Loader/Loader";
 import Image from "next/image";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import toast from "react-hot-toast";
@@ -30,6 +31,11 @@ const CauseDetailsPage = ({ params }) => {
 
     const number = event.target.number.value;
     const donate = {
+      causeId: data?._id,
+      title: data?.title,
+      img: data?.primary_img,
+      description: data?.description,
+      causeName: data?.name,
       name,
       email: user.email,
       number,
@@ -114,97 +120,107 @@ const CauseDetailsPage = ({ params }) => {
         </div>
 
         {/* Donation form */}
-        <div className="flex flex-col max-w-xl p-6  mx-auto  my-20 rounded-md sm:p-10 bg-[#FEF2F1] text-gray-800">
-          <div className="mb-8 ">
-            <h1 className="my-3 text-4xl font-bold">Donate!</h1>
-            <p className="text-sm text-gray-600">
-              Donate to this cause and help us
-            </p>
-          </div>
-          <p className="mb-2">Fill Personal Info:</p>
-          <form onSubmit={handleDonation} className="space-y-12">
-            <div className="space-y-4">
-              <div>
-                <label for="email" className="block mb-2 font-semibold">
-                  Full Name
-                </label>
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  required
-                  placeholder="your name"
-                  className="w-full px-3 py-3 border rounded-md border-gray-300 bg-gray-50 text-gray-800"
-                />
+        {user?.email ? (
+          <div className="flex flex-col max-w-xl p-6  mx-auto  my-20 rounded-md sm:p-10 bg-[#FEF2F1] text-gray-800">
+            <div className="mb-8 ">
+              <h1 className="my-3 text-4xl font-bold">Donate!</h1>
+              <p className="text-sm text-gray-600">
+                Donate to this cause and help us
+              </p>
+            </div>
+            <p className="mb-2">Fill Personal Info:</p>
+            <form onSubmit={handleDonation} className="space-y-12">
+              <div className="space-y-4">
+                <div>
+                  <label for="email" className="block mb-2 font-semibold">
+                    Full Name
+                  </label>
+                  <input
+                    id="name"
+                    name="name"
+                    type="text"
+                    required
+                    placeholder="your name"
+                    className="w-full px-3 py-3 border rounded-md border-gray-300 bg-gray-50 text-gray-800"
+                  />
+                </div>
+
+                <div>
+                  <div>
+                    <label for="number" className="font-semibold block mb-2">
+                      Mobile Number
+                    </label>
+                  </div>
+                  <input
+                    type="number"
+                    name="number"
+                    placeholder="your number"
+                    className="w-full px-3 py-3 border rounded-md border-gray-300 bg-gray-50 text-gray-800"
+                  />
+                </div>
               </div>
 
               <div>
-                <div>
-                  <label for="number" className="font-semibold block mb-2">
-                    Mobile Number
-                  </label>
+                <p className="font-semibold  ">Donate Amount</p>
+                <div className="flex items-center  gap-6 my-6 mx-auto ">
+                  <p
+                    className="bg-gray-200 px-3 hover:text-white duration-300  hover:font-semibold hover:bg-secondary py-1.5 rounded"
+                    onClick={() => setAmount(100)}
+                  >
+                    100
+                  </p>
+                  <p
+                    className="bg-gray-200 px-3 hover:text-white duration-300  hover:font-semibold hover:bg-secondary py-1.5 rounded"
+                    onClick={() => setAmount(200)}
+                  >
+                    200
+                  </p>
+                  <p
+                    className="bg-gray-200 px-3 hover:text-white duration-300  hover:font-semibold hover:bg-secondary py-1.5 rounded"
+                    onClick={() => setAmount(500)}
+                  >
+                    500
+                  </p>
+                  <p
+                    className="bg-gray-200 px-3 hover:text-white duration-300  hover:font-semibold hover:bg-secondary py-1.5 rounded"
+                    onClick={() => setAmount(1000)}
+                  >
+                    1000
+                  </p>
                 </div>
                 <input
                   type="number"
-                  name="number"
-                  placeholder="your number"
+                  name="amount"
+                  placeholder="amount"
+                  required
                   className="w-full px-3 py-3 border rounded-md border-gray-300 bg-gray-50 text-gray-800"
+                  value={amount}
+                  onChange={(e) => setAmount(parseInt(e.target.value))}
                 />
               </div>
-            </div>
 
-            <div>
-              <p className="font-semibold  ">Donate Amount</p>
-              <div className="flex items-center  gap-6 my-6 mx-auto ">
-                <p
-                  className="bg-gray-200 px-3 hover:text-white duration-300  hover:font-semibold hover:bg-secondary py-1.5 rounded"
-                  onClick={() => setAmount(100)}
-                >
-                  100
-                </p>
-                <p
-                  className="bg-gray-200 px-3 hover:text-white duration-300  hover:font-semibold hover:bg-secondary py-1.5 rounded"
-                  onClick={() => setAmount(200)}
-                >
-                  200
-                </p>
-                <p
-                  className="bg-gray-200 px-3 hover:text-white duration-300  hover:font-semibold hover:bg-secondary py-1.5 rounded"
-                  onClick={() => setAmount(500)}
-                >
-                  500
-                </p>
-                <p
-                  className="bg-gray-200 px-3 hover:text-white duration-300  hover:font-semibold hover:bg-secondary py-1.5 rounded"
-                  onClick={() => setAmount(1000)}
-                >
-                  1000
-                </p>
+              <div className="space-y-2">
+                <div>
+                  <button
+                    type="submit"
+                    disabled={isLoading}
+                    className="w-full px-8 py-3 font-semibold border-0 shadow-md rounded-md bg-secondary hover:bg-red-500 text-gray-50"
+                  >
+                    {isLoading ? "Donating..." : "Donate"}
+                  </button>
+                </div>
               </div>
-              <input
-                type="number"
-                name="amount"
-                placeholder="amount"
-                required
-                className="w-full px-3 py-3 border rounded-md border-gray-300 bg-gray-50 text-gray-800"
-                value={amount}
-                onChange={(e) => setAmount(parseInt(e.target.value))}
-              />
-            </div>
-
-            <div className="space-y-2">
-              <div>
-                <button
-                  type="submit"
-                  disabled={isLoading}
-                  className="w-full px-8 py-3 font-semibold border-0 shadow-md rounded-md bg-secondary hover:bg-red-500 text-gray-50"
-                >
-                  {isLoading ? "Donating..." : "Donate"}
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
+            </form>
+          </div>
+        ) : (
+          <h2 className=" font-semibold text-2xl my-4 text-center">
+            Please{" "}
+            <Link href={"/login"} className="text-secondary ">
+              Login
+            </Link>{" "}
+            to donate!
+          </h2>
+        )}
       </div>
     </div>
   );
